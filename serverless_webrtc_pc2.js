433299af5f5e5c39267e78e79ce7cb34c0b1f915
Deep Kotadia <deep.kotadia@rutgers.edu>
2016-03-02 14:52:58 -0500

    Update serverless_webrtc_pc2.js

--- serverless_webrtc_pc2.js
+++ serverless_webrtc_pc2.js
@@ -1,3 +1,10 @@
+navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+
+function receive(){
+
+Omer.disabled = true;
+hangupButton.disabled = false;
 
 navigator.getMedia({
 
@@ -5,3 +12,11 @@ navigator.getMedia({
 	audio : false
 
 })
+
+}
+
+function hangup(){
+	Deep.disabled = true;
+	hangupButton.disabled = true;
+	Omer.disabled = false;
+}
